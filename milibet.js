b6493d93b83c52716e4dc57513d301f92df16a11
Jonathan Azoff <jon@azoffdesign.com>
2011-08-11 16:07:23 -0700

    first commit

---
+++ milibet.js
@@ -0,0 +1,53 @@
+var url = require('url'),
+
+port = 1337,
+
+alphabet = {
+    a: 'Alpha',
+    b: 'Bravo',
+    c: 'Charlie',
+    d: 'Delta',
+    e: 'Echo',
+    f: 'Foxtrot',
+    g: 'Golf',
+    h: 'Hotel',
+    i: 'India',
+    j: 'Juliet',
+    k: 'Kilo',
+    l: 'Lima',
+    m: 'Mike',
+    n: 'November',
+    o: 'Oscar',
+    p: 'Papa',
+    q: 'Quebec',
+    r: 'Romeo',
+    s: 'Sierra',
+    t: 'Tango',
+    u: 'Uniform',
+    v: 'Victor',
+    w: 'Whiskey',
+    x: 'X-ray',
+    y: 'Yankee',
+    z: 'Zulu'
+};
+
+function militerize(input) {
+    var i, key, output = '';
+    for (i=0; i<input.length; i++) {
+        key = input.substr(i,1).toLowerCase();
+        output += alphabet[key];
+    }
+    return output;
+}
+
+function server(req, res) {
+    var input = url.parse(req.url).pathname.substr(1);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(militerize(input));
+}
+
+if (process.argv.length > 2) {
+    console.log(militerize(process.argv[2]));
+} else {
+    require('http').createServer(server).listen(port, "127.0.0.1");
+}
